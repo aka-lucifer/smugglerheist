799a2163f66a6cce34a08c2165c5a7d2ff3bfa20
akaLucifer <34045997+aka-lucifer@users.noreply.github.com>
2024-12-03 22:10:57 +0000

    chore(config) adjusted Z coords in config

--- config/server.lua
+++ config/server.lua
@@ -1,10 +1,10 @@
 return {
-    spawnCoords = vec4(-309.05, 7903.28, 1418.78, 170.4),
+    spawnCoords = vec4(-309.05, 7903.28, 600.78, 170.4),
     crateOffsets = {
         vec3(-1.7, 25.7, 0.0), -- FL
         vec3(1.6, 26.0, 0.0), -- FR
         vec3(-1.9, 2.4, 0.0), -- RL
         vec3(1.9, 2.5, 0.0) -- RR
     },
-    destinationCoords = vec3(-1325.82, -5204.93, 340.37), -- Where the plane will fly to, if it reaches here mission will cancel
+    destinationCoords = vec3(-1325.82, -5204.93, 866.37), -- Where the plane will fly to, if it reaches here mission will cancel
 }
