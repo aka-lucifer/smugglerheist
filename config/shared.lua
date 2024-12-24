3cadef495bba42081bed4509c62f8de1575ddb61
akaLucifer <34045997+aka-lucifer@users.noreply.github.com>
2024-12-24 12:25:41 +0000

    chore(config/shared) update bomb attach pos

--- config/shared.lua
+++ config/shared.lua
@@ -3,11 +3,11 @@ return {
     hackDistance = 200.0, -- How close you have to be for hacking to be viable
     bombPlacementOffsets = {
         { -- Left Wing
-            attach = vec3(27.0, 2.0, 1.4),
+            attach = vec3(29.8, 2.0, 1.2),
             explode = vec3(-29.0, -7.0, 1.16)
         },
         { -- Right Wing
-            attach = vec3(-27.0, 13.0, 1.3),
+            attach = vec3(-27.5 , 11.5, 1.3),
             explode = vec3(29.0, -7.0, 1.16)
         },
     },
